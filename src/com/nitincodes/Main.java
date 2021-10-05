5bc6ff3375afc97c9cd5810f225ab0b462e58feb
Nitin <nsinghboss02@gmail.com>
2021-10-05 19:31:35 +0530

    Initial commit

---
+++ src/com/nitincodes/Main.java
@@ -0,0 +1,11 @@
+package com.nitincodes;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// java program to find the index of a string:
+    String name = "Nitin";
+    int n = name.indexOf('i');
+        System.out.println(n);
+    }
+}
